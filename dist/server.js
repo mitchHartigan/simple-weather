e4fd9dde36770ce41642b36756695840da7e09cf
Mitch Hartigan <42709932+mitchHartigan@users.noreply.github.com>
2024-02-25 11:37:54 -0800

    feat: handlebars install

--- dist/server.js
+++ dist/server.js
@@ -5,15 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
+const express_handlebars_1 = require("express-handlebars");
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
 app.use(express_1.default.json());
 app.use(body_parser_1.default.json({ limit: "50mb" }));
 app.use(body_parser_1.default.urlencoded({ limit: "50mb", extended: true }));
-app.use(express_1.default.static("public"));
-app.get("/clicked", (req, res) => {
-    console.log("req", req.body);
-    res.sendFile("views/component.html", { root: __dirname });
+// app.use(express.static("public"));
+app.engine("hbs", (0, express_handlebars_1.engine)({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+app.get("/", (req, res) => {
+    res.render("layouts/main", {
+        title: "My test page ayy lmao",
+        message: "Welcome brothers",
+    });
 });
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
+import { engine } from "express-handlebars";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,11 +8,17 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(bodyParser.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-app.get("/clicked", (req, res) => {
-  console.log("req", req.body);
-  res.sendFile("views/component.html", { root: __dirname });
+app.engine("hbs", engine({ defaultLayout: "main", extname: ".hbs" }));
+app.set("view engine", "hbs");
+app.set("views", __dirname + "/views");
+
+app.get("/", (req, res) => {
+  res.render("layouts/main", {
+    title: "My test page ayy lmao",
+    message: "Welcome brothers",
+  });
 });
 
 app.listen(port, () => {
