17234f6591a09dce285c2b94925c82bae2c6fdcf
Mitch Hartigan <42709932+mitchHartigan@users.noreply.github.com>
2024-02-24 19:11:47 -0800

    feat: working htmx-get

--- dist/server.js
+++ dist/server.js
@@ -4,9 +4,16 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const body_parser_1 = __importDefault(require("body-parser"));
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
+app.use(express_1.default.json());
+app.use(body_parser_1.default.json({ limit: "50mb" }));
+app.use(body_parser_1.default.urlencoded({ limit: "50mb", extended: true }));
 app.use(express_1.default.static("public"));
+app.get("/clicked", (req, res) => {
+    res.send(`<h1>Ayy lmao</h1>`);
+});
 app.listen(port, () => {
     console.log(`Server listening on port ${port}`);
 });
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
 import express from "express";
-import path from "path";
+import bodyParser from "body-parser";
 
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+app.use(bodyParser.json({ limit: "50mb" }));
+app.use(bodyParser.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.static("public"));
+
+app.get("/clicked", (req, res) => {
+  res.send(`<h1>Ayy lmao</h1>`);
+});
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
